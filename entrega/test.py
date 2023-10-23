2e3cc27c24d8f517c835c06f8d22af659974f2e1
Facundo7777 <147439754+Facundo7777@users.noreply.github.com>
2023-10-23 18:58:29 +0000

    actualizar

--- entrega/test.py
+++ entrega/test.py
@@ -1,1 +1,14 @@
-print(hello world)
+
+print(2+3)
+
+
+
+
+
+
+#argumentos posicionales
+#print(argumentos posicionales,argumentos reservados)
+#literales : 
+# numeros  : int,float     2_3.0 o 23
+#booleanos : True False
+#cadenas : ''   ""  """
