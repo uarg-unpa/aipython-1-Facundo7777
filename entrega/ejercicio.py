cee16830a4792b4c35f2da96615ae3a75b2c4ae5
Facundo7777 <147439754+Facundo7777@users.noreply.github.com>
2023-10-30 19:02:17 +0000

    actualizar

--- entrega/ejercicio.py
+++ entrega/ejercicio.py
@@ -2,5 +2,10 @@ mi_texto = "hola_mundo"
 mi_texto = mi_texto.replace("o",":)")
 print(mi_texto)
 
-edad = 10
-print(F "Hello World{edad}")
+#edad = 10
+#print(f "Hello World{edad}")
+
+mi_nombre = alan_becker
+print(mi_nombre[0;3])
+
+
